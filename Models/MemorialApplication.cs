30745ebac3415f71d9df550fe24cc0b934a37ab5
TDahman <TDahman@09-TDAHMAN-W10>
2020-07-27 14:57:07 -0500

    Added class for applications and folder for scaffolding views.

---
+++ Models/MemorialApplication.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Centralization.Models
+{
+    public class MemorialApplication
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        public string FilePath { get; set; }
+
+        public ApplicationType Type { get; set; }
+
+        public DateTime UploadDate { get; set; }
+
+        public string UploadedBy { get; set; }
+
+        public Interment Interred { get; set; }
+    }
+
+    public enum ApplicationType
+    {
+        OptionalCompleteService = 0,
+        CompleteCemeteryService = 1,
+        CryptPlaque = 2,
+        NichePlaque = 3
+    }
+}
